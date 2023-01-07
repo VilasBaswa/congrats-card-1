91374a48768ec2e7d439e9514525bff943a8f448
Vilas Baswa <baswavilas2000@gmail.com>
2023-01-07 14:35:02 +0000

    congrats card

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+  <div className="congratulations-card-container">
+    <h1 className="heading">Congratulations</h1>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
